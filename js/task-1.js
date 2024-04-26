db964bfcdb3b2fc8c5590333180e1f8eea363d20
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-04-26 12:21:42 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+// Генератор slug
+function slugify(title) {
+  let slug = "";
+  for (let i = 0; i < title.length; i++) {
+    title[i] === " " ? (slug += "-") : (slug += title[i].toLowerCase());
+  }
+  return slug;
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
